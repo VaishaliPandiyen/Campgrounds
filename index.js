16560e87b4cf9e2a14c423209781aaaa6bd0482a
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2022-06-24 12:58:50 +0530

    Added some styles

--- index.js
+++ index.js
@@ -1,79 +1,91 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const path = require('path');
-const Campground = require('./models/campground');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const ejsMate = require('ejs-mate');
+const path = require("path");
+const Campground = require("./models/campground");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/YelpCamp',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/YelpCamp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error:"));
 db.once("open", () => {
-    console.log("Database Connected!");
-})
-
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(morgan('tiny'));
-
-app.get('/', (req, res) => {
-    res.render('home')
+  console.log("Database Connected!");
 });
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-})
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/campgrounds/new', async (req, res) => {
-    res.render('campgrounds/new')
-})
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(morgan("tiny"));
 
-app.post('/campgrounds', async (req, res) => {
-    const newCampground = new Campground(req.body.campground);
-    await newCampground.save()
-    res.redirect(`/campgrounds/${newCampground._id}`)
-})
+app.get("/", (req, res) => {
+  res.render("campgrounds/landing");
+});
 
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/details', { campground });
-})
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', { campground });
-})
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true, new: true });
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+app.post("/campgrounds", async (req, res) => {
+  const newCampground = new Campground(req.body.campground);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+});
 
-app.delete('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedCampground = await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-})
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/details", { campground });
+});
 
-app.get('/sampleCampground', async (req, res) => {
-    const camp1 = new Campground({ title: 'Elliots Beach retro', price: 'Rs.1,000', description: 'No fishing allowed', location: 'Elliots Beach, Chennai' });
-    await camp1.save();
-    res.send(camp1);
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    req.body.campground,
+    { runValidators: true, new: true }
+  );
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedCampground = await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
+
+app.get("/sampleCampground", async (req, res) => {
+  const camp1 = new Campground({
+    title: "Elliots Beach retro",
+    price: "Rs.1,000",
+    description: "No fishing allowed",
+    location: "Elliots Beach, Chennai",
+  });
+  await camp1.save();
+  res.send(camp1);
 });
 
 app.listen(3030, () => {
-    console.log("On port 3030")
+  console.log("On port 3030");
 });
--- seeds/indexSeeds.js
+++ seeds/indexSeeds.js
@@ -1,35 +1,40 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
 
-mongoose.connect('mongodb://localhost:27017/YelpCamp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost:27017/YelpCamp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const cost = Math.floor(Math.random() * 300);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      price: cost,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/1846775",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, dolorum. Facere quisquam quas ipsa eaque deleniti iusto provident aliquam maiores corporis omnis perspiciatis dolorem, repudiandae odit praesentium, adipisci tenetur fuga.",
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
